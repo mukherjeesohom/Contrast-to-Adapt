1196036b2212069fd064ffa08c2a93c00818ccb5
nik1806 <nikhil.paliwal14@gmail.com>
2021-07-17 11:19:32 +0200

    saving intermediate changes for noisy label gen

--- train_model.py
+++ train_model.py
@@ -114,8 +114,8 @@ if __name__ == '__main__':
                         # help='epoch to resume')
     parser.add_argument('--save_epoch', type=int, default=10, metavar='N',
                         help='when to restore the model')
-    parser.add_argument('--save_model', action='store_true', default=False,
-                        help='save_model or not')
+    # parser.add_argument('--save_model', action='store_true', default=False,
+    #                     help='save_model or not')
     parser.add_argument('--seed', type=int, default=1, metavar='S',
                         help='random seed (default: 1)')
     parser.add_argument('--source', type=str, default='svhn', metavar='N',
